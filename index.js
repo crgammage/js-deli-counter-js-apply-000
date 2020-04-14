8a2c7c1841860f59cd8e5bacf681e5d0c79d04a3
masterful-array-3850 <crgammage@gmail.com>
2020-04-14 20:33:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ function nowServing(katzDeli) {
     return `There is nobody waiting to be served.`
   }
 }
+
+function currentLine(katzDeli) {
+  var output = [];
+  if (katzDeli.length > 0) {
+    for (var i = 0; i < katzDeli.length; i++) {
+      output += (i + 1) + "." + katzDeli[i] + ","
+    }
+    output = output.slice(0, output.length - 2)
+    return "The line is currently:" + output;
+  } else {
+    return "The line is currently empty."
+  }
+}
