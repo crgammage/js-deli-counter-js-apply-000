9e48fc0bafd8d159fc850166298988a4dba65d79
masterful-array-3850 <crgammage@gmail.com>
2020-04-14 20:29:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var katzDeli = [];
 function takeANumber(katzDeli, name) {
   katzDeli.push(${name});
-  return (`Welcome ${name}` you are number ${katzDeli.length} in line`)
+  return (`Welcome ${name} you are number ${katzDeli.length} in line`)
+}
+
+function nowServing(katzDeli) {
+  if (katzDeli.length > 0) {
+    return "Currently serving" + katzDeli.shift() + "."
+  }
+  else {
+    return "There is nobody waiting to be served."
+  }
 }
